70562e78c34a0e6e05ed5477584f8178175da94d
Megha Ranjith <115008689+megha-ranjith@users.noreply.github.com>
2025-04-01 21:10:33 +0530

    Update emotion.py

--- emotion.py
+++ emotion.py
@@ -1,7 +1,7 @@
 import cv2
 import mediapipe as mp
 import numpy as np 
-
+ 
 # Initialize Mediapipe FaceMesh and Drawing Utilities
 mp_drawing = mp.solutions.drawing_utils
 mp_drawing_styles = mp.solutions.drawing_styles
